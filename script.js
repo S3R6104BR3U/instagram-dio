ac74fccf67d3c3c3ff887e96274c3f9e40747dc0
S3R6104BR3U <sergioabreusousa@gmail.com>
2022-07-14 15:33:41 -0300

    add modo escuro links e menu select de idiomas

--- script.js
+++ script.js
@@ -27,31 +27,19 @@ const intervalo = window.setInterval(function slid() {
 
         } , 2000);
 
- /* var i;
-i=1;
 
- window.onclick = function slid() {
-          
-          if(i === 1){
-            document.getElementById("print").src = "./img/print1.png";
-          }
+ /* mudar tema */
+var white=true;
+function tema(){
+  if(white===true){
+    document.getElementById("style").href="style-escuro.css";
+    document.getElementById("btn-tema").value="Tema Claro";
+    white=false;
+  }
 
-          else if(i === 2){
-            document.getElementById('print').src = "./img/print2.png";
-          }
-
-          else if(i === 3){
-            document.getElementById("print").src = "./img/print3.png";
-          }
-
-          else if(i === 4){
-            document.getElementById("print").src = "./img/print4.png";
-          }
-
-          i++;
-
-          if(i>4){
-            i=1;
-          }
-
-        } */
+  else{
+    document.getElementById("style").href="style.css";
+    document.getElementById("btn-tema").value="Tema Escuro";
+    white=true;
+  }
+}
